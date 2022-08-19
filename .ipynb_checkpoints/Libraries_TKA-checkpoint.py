ce6a64f83e8eec97c04ba2adf315ea47bacb7ef7
Nick Littlefield <nickolas.littlefield@maine.edu>
2022-08-19 10:57:28 -0400

    Filtered cohort CSV

---
+++ .ipynb_checkpoints/Libraries_TKA-checkpoint.py
@@ -0,0 +1,15 @@
+import argparse
+
+import h5py
+
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+import shutil
+
+import pydicom as dicom # pip install pydicom
+import cv2 # pip install opencv-python
---
+++ .ipynb_checkpoints/Main_TKA-checkpoint.py
@@ -0,0 +1,11 @@
+from Param_TKA import *
+from Libraries_TKA import *
+
+import lookup as lookup
+import cohort_finder as cohort_finder
+# import cohort_finder2 as cohort_finder2
+
+
+# lookup.main()
+cohort_finder.main()
+
